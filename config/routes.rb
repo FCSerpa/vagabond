ba9016c860348554a899dc98609e8c1f0f02162d
shiva subedi <trsubedi@hotmail.com>
2015-09-28 16:47:47 -0700

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 #           DELETE /users/:id(.:format)      users#destroy
   get '/users/new', to: 'users#new' 
   post '/users', to: 'users#create'
-  get 'users/:id' to: 'users#show'
+  get 'users/:id', to: 'users#show'
 
 end
