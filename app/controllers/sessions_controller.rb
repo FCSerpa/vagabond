b7994eda2f20588eb8d86a8915175f40a417c4ea
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-09-28 17:59:06 -0700

    merging local and remote develop branch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user_params = params.require(:user).permit(:email, :password)
-		@user = User.confirm(user_params)
-		if @user
-	end
+	    user_params = params.require(:user).permit(:email, :password)
+	    @user = User.confirm(user_params)
+	    if @user
+	      login(@user)
+	      redirect_to "/users/#{@user.id}"
+	    else
+	      redirect_to "/sign_in"
+	    end
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   get '/users/new', to: 'users#new' 
   post '/users', to: 'users#create'
-  get 'users/:id' to: 'users#show'
+  get 'users/:id', to: 'users#show'
 
 end
