3617d7bcd95959844f03b64befd61bd5496ce56b
Keala Lusk <contactkeala@gmail.com>
2015-09-28 19:23:22 -0700

    added ffaker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-db
+
 User.create({first_name: "Harry", last_name: "Potter", email: "potter@wizard.com"}) 
 User.create({first_name: "Hermione", last_name: "Granger", email: "granger@witch.com"}) 
 User.create({first_name: "Ron", last_name: "Weasley", email: "weasley@wizard.com"})
 
-Tips.create({name: "Hogsmeade", description: "the only all-wizarding village in Britain", users_id: 3})
+Tip.create({name: "Hogsmeade", description: "The only all-wizarding village in Britain", users_id: 3})
