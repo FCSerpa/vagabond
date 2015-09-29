302e19a79d6607fa59c309109003608f2a5d708d
Keala Lusk <contactkeala@gmail.com>
2015-09-29 11:54:27 -0700

    fixed css

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
   get 'tips/new'
-
   get 'tips/show'
+  get '/tips/:id', to: 'tips#show'
 
-  	get "/sign_in", to: "sessions#new"
-  	post "/sessions/new", to: "sessions#create"
+  get "/sign_in", to: "sessions#new"
+  post "/sessions/new", to: "sessions#create"
 
-	root 'welcome#index'
-	get 'user/new'
+  get 'user/new'
+  get '/users/new', to: 'users#new' 
+  post '/users', to: 'users#create'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id/edit', to: 'users#edit'
+  patch 'users/:id', to: 'users#update'
 
-  	get '/users/new', to: 'users#new' 
-  	post '/users', to: 'users#create'
-  	get 'users/:id', to: 'users#show', as: 'user'
-  	get 'users/:id/edit', to: 'users#edit'
-  	patch 'users/:id', to: 'users#update'
-
-
-  	get '/tips/:id', to: 'tips#show'
+  get '/places/:id', to: 'placesw#show'
 
 end
