ceb14af82e5a1149b52138aad8e2f92a84fd05c8
shiva subedi <trsubedi@hotmail.com>
2015-09-30 09:35:26 -0700

    edit form for post

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'tips/new'
   get 'tips/show'
   get '/tips/:id', to: 'tips#show'
+  get 'tips/:id/edit', to: 'tips#edit', as: 'tip'
+  patch '/tips/:id', to: 'tips#update'
 
   get "/sign_in", to: "sessions#new"
   post "/sessions/new", to: "sessions#create"
