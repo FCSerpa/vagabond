c74199b04588574bbdd39cd5efe590af2fd8b342
Francis Serpa <fcserpa@gmail.com>
2015-09-30 07:51:05 -0700

    merging

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,8 @@
 class PlacesController < ApplicationController
 	def show
 		@place = Place.find_by({id: params[:id]})
-		@tip = Tip.where(places_id: params[1])
-		@tip.each do |f|
-		end
+		@tip = Tip.where(place_id: params[:id])
+		# @tip.each do |f|
+		# end
 	end
 end
