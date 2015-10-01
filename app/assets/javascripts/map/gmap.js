da927e629bc8e13a8b58bbedb07a8fe3ae821c7e
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-09-30 18:54:35 -0700

    Google map and markers are up and working successfully

---
+++ app/assets/javascripts/map/gmap.js
@@ -0,0 +1,35 @@
+var sf_lat = 37.683;
+var sf_lng = -122.416;
+var london_lat = 51.507;
+var london_lng = -0.1275;
+var gib_lat = 36.143;
+var gib_lng = -5.353;
+
+$(document).ready(function(){
+	ourMap = createMap();
+	createMarker(sf_lat, sf_lng, ourMap, "San Francisco");
+	createMarker(london_lat, london_lng, ourMap, "London");
+	createMarker(gib_lat, gib_lng, ourMap, "Gibralter");
+
+	
+});
+
+function createMap()
+{
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+	{
+		center: {lat: 48.9271, lng: -36.263},
+		zoom: 3
+	});
+
+	return map;
+}
+
+function createMarker(lat, lng, map, title)
+{
+	new google.maps.Marker({
+		position: new google.maps.LatLng(lat,lng),
+		map: map,
+		title: title
+	});
+}
